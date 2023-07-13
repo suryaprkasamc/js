e9c33fc694ab14806cc561c70716699f307e1db6
suryaprakasamc <suryaprakasam@innoboon.com>
2023-07-13 17:31:07 +0530

    single link list

---
+++ dsCodeEval/sll.js
@@ -0,0 +1,14 @@
+class SingleLinkList{
+    constructor(){
+
+    }
+    insert(n){}
+    delete(n){}
+    find(n){}
+    print(){
+        console.log('List  is')
+    }
+}
+
+let list1 = new SingleLinkList()
+list1.print()
